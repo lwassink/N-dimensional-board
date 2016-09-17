c0c4198fd311bf8f35e528223d94de27004ae42d
Luke Wassink <lwassink@gmail.com>
2016-09-17 15:53:36 -0700

    add Board#render, Board#to_s, and Board#inspect

--- board.rb
+++ board.rb
@@ -15,7 +15,10 @@ class Board
   def initialize(dimensions = [8,8])
     @dimensions = dimensions
     @data = Array.new(size)
-    @dim = @dimensions.length
+  end
+
+  def dim
+    @dimensions.length
   end
 
   def size
@@ -30,14 +33,52 @@ class Board
     @data[index(pos)] = value
   end
 
+  def positions
+    size.times.map { |idx| position(idx) }
+  end
+
+  def each_with_pos(&prc)
+    positions.each do |pos|
+      prc.call(self[pos], pos)
+    end
+  end
+
+  def render
+    case dim
+    when 0
+      "empty board"
+    when 1
+      @data.map(&:to_s).join(' ')
+    when 2
+      @dimensions[0].times.map do |row|
+        @dimensions[1].times.map { |col| self[[row, col]].to_s }.join(' ')
+      end.join("\n")
+    else
+      raise "#render can only print a board of dim 2 or less"
+    end
+  end
+
+  def to_s
+    return render if dim <= 2
+
+    "Dimensions: #{@dimensions.to_s}"
+  end
+
+  def inspect
+    "Board: #{self.object_id}\n" +
+      "Size: #{size.inspect}, dimension: #{dim.inspect}\n" +
+      "Dimensions: #{@dimensions.inspect}"
+  end
+
+  private
 
   # return the index in @data pecified by a postion vector
   # used by the [] and []= methods
   def index(pos)
     idx = 0
 
-    @dim.times do |i|
-      if i < @dim - 1
+    dim.times do |i|
+      if i < dim - 1
         idx += pos[i] * @dimensions.drop(i + 1).reduce(&:*)
       else
         idx += pos[i]
@@ -51,8 +92,8 @@ class Board
   def position(idx)
     pos = []
 
-    @dim.times do |i|
-      if i < @dim - 1
+    dim.times do |i|
+      if i < dim - 1
         row_length = @dimensions.drop(i+1).reduce(&:*)
         pos << idx / row_length
         idx -= pos.last * row_length
@@ -62,16 +103,6 @@ class Board
     end
 
     pos
-  end
-
-  def positions
-    (0...size).map { |idx| position(idx) }
-  end
-
-  def each_with_pos(&prc)
-    positions.each do |pos|
-      prc.call(self[pos], pos)
-    end
   end
 end
 
